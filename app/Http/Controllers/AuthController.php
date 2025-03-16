910e2e2895b16bdd86560e86093d5205d0d09ff0
Pedro Gonzalez <pedrogdevv@gmail.com>
2025-03-16 19:53:10 +0100

    Base del proyecto ya conectado a el frontend y creando users

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        //Validamos los datos que nos llegan
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6',
+        ]);
+
+        //Creamos el usuario en la base de datos
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        //Generamos un token para el usuario
+        //$token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'Usuario creado correctamente',
+        ], 201);
+
+
+    }
+}
